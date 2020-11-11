e0503b82a79898e72ec3526136155a09e50318b8
Mike Kim <mikekim.info@gmail.com>
2020-11-11 11:35:18 -0500

    feat: CRUD api routes  and add/delete for products

--- server/api/products.js
+++ server/api/products.js
@@ -13,9 +13,47 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:productId', async (req, res, next) => {
   try {
-    const products = await Product.findByPk(req.params.productId)
-    res.json(products)
+    const product = await Product.findByPk(req.params.productId)
+    if (!product) {
+      return res.sendStatus(404)
+    }
+    res.json(product)
   } catch (err) {
     next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const product = await Product.create(req.body)
+    res.status(201).json(product)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:productId', async (req, res, next) => {
+  try {
+    const id = +req.params.productId
+    await Product.destroy({
+      where: {id}
+    })
+    res.sendStatus(204)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:productId', async (req, res, next) => {
+  try {
+    const id = +req.params.productId
+    const product = await Product.findByPk(id)
+    if (!product) {
+      res.sendStatus(404)
+    }
+    const updatedProduct = await product.update(req.body, {returning: true})
+    res.json(updatedProduct)
+  } catch (error) {
+    next(error)
   }
 })
