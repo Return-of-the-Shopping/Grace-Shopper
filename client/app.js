489c066d34403179c8ed5d099180b511587d2c63
Tun Oo Swe <tunooswe@gmail.com>
2020-11-12 11:44:36 -0500

    carousel added

--- client/app.js
+++ client/app.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
+import HomePage from './components/home-page'
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Routes />
+      {/* <Routes /> */}
+      <HomePage />
     </div>
   )
 }
