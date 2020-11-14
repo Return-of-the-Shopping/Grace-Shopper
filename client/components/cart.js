5c5057f6b8ca8cbef032446b4ae19004ea7fc733
Tun Oo Swe <tunooswe@gmail.com>
2020-11-14 15:30:42 -0500

    cart features completed

--- client/components/cart.js
+++ client/components/cart.js
@@ -5,24 +5,43 @@ import cart from '../cart'
 import ProductLine from './product-line'
 
 class Cart extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      orderTotal: 0,
+      cartUpdate: false
+    }
+
+    this.resetCartState = this.resetCartState.bind(this)
+  }
+
+  resetCartState(state) {
+    this.setState({cartUpdate: !state.cartUpdate})
+  }
+
   render() {
     return (
       <div>
-        {Object.keys(cart).map(productId => {
-          const info = {
-            userId: this.props.user.id,
-            productId
-          }
-          return (
-            <ProductLine
-              product={JSON.parse(cart[productId])}
-              info={info}
-              editCart={this.props.editCart}
-              key={productId}
-              cart={cart}
-            />
-          )
-        })}
+        <div>
+          {Object.keys(cart).map(productId => {
+            const info = {
+              userId: this.props.user.id,
+              productId
+            }
+            return (
+              <ProductLine
+                product={JSON.parse(cart[productId])}
+                info={info}
+                editCart={this.props.editCart}
+                removeCart={this.props.removeCart}
+                key={productId}
+                cart={cart}
+                resetCartState={() => this.resetCartState(this.state)}
+              />
+            )
+          })}
+        </div>
+        <div />
       </div>
     )
   }
